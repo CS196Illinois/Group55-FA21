8cdb4c2415e2c2942d581702d8d424d03e414672
shrevin <shreya.vinjamuri@gmail.com>
2021-12-02 16:54:41 -0600

    added more crn boxes and a map

--- Project/frontend/app.py
+++ Project/frontend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, render_template
 from flask import Flask
+
 # from Project.frontend.backend import generate_daily_agenda
 from backend import generate_daily_agenda, extract_course_data, organize_classes, convert_times_and_sort_days, convert_to_datetime, finalClasses
 import folium
 app = Flask(__name__)
 
+from geopy.geocoders import Nominatim
+geocoder = Nominatim(user_agent = app)
 # @app.route("/")
 # def base():
 #   map = folium.Map(
@@ -29,27 +32,30 @@ def firstPagePost():
     crns.append(request.form['crn4'])
   if (len(request.form['crn5']) == 5):
     crns.append(request.form['crn5'])
-  # if (len(request.form['crn6']) == 5):
-  #   crns.append(request.form['crn6'])
+  if (len(request.form['crn6']) == 5):
+    crns.append(request.form['crn6'])
+  if (len(request.form['crn7']) == 5):
+    crns.append(request.form['crn7'])
+  if (len(request.form['crn8']) == 5):
+    crns.append(request.form['crn8'])
   
   if len(crns) > 0:
-    # course_data = extract_course_data(crns)
-    # days = {"M" : [], "T" : [], "W": [], "R": [], "F" : []}
-    # generate_daily_agenda(days, course_data)
-    # sorted_day = {}
-    # classes_datetime = {"M" : [], "T" : [], "W": [], "R": [], "F" : []}
-    # test = {"M" : [], "T" : [], "W": [], "R": [], "F" : []}
-    # convert_times_and_sort_days(days, sorted_day)
-    # convert_to_datetime(classes_datetime, sorted_day)
-    # organize_classes(test, classes_datetime)
-    # print(test)
-    #crnsInput = {'59821', '75111', '73308', '75272', '48924', '70667', '62829', '57971'}
     test = finalClasses(crns)
+    # '59821', '75111', '73308', '75272', '48924', '70667', '62829', '57971'
     print(test)
-    # map = folium.Map(
-    # width=50,height=50, location=[45, -122]
-    # )
-    return render_template('enterCRN.html', test = test, crns = crns) #, map._repr_html_()
+    # location = geocode("Bevier Hall UIUC")
+    # print(location)
+    map = folium.Map(
+    width=1000,height=1000, location=[45, -122]
+    )
+    loc = [(40.720, -73.993),
+       (27.205, 77.498)]
+    folium.PolyLine(loc,
+                color='red',
+                weight=15,
+                opacity=0.8).add_to(map)
+    return map._repr_html_()
+    # return render_template('enterCRN.html', test = test, crns = crns) #, map._repr_html_()
     
   else: 
     return render_template('enterCRN.html')
